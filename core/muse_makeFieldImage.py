01dbceb25efb481ac7e0b940d36362f9449286b0
Will Liu <49797040+Sullofect@users.noreply.github.com>
2022-05-17 15:48:59 -0400

    -update

--- core/muse_makeFieldImage.py
+++ core/muse_makeFieldImage.py
@@ -24,69 +24,16 @@ newcolors = Blues(np.linspace(0, 1, 256))
 newcolors_red = Reds(np.linspace(0, 1, 256))
 newcmp = ListedColormap(newcolors)
 
-
-path_1 = os.path.join(os.sep, 'Users', 'lzq', 'Dropbox', 'redshifting', 'ESO_DEEP_offset_zapped_spec1D',
-                      '1_20002_J024034.53-185135.09_spec1D.fits')
-spec = Table.read(path_1)
-wave_1 = pyasl.vactoair2(spec['wave'])
-flux_1 = spec['flux'] * 1e-3
-model_1 = spec['model'] * 1e-3
-flux_err_1 = spec['error'] * 1e-3
-
-path_2 = os.path.join(os.sep, 'Users', 'lzq', 'Dropbox', 'redshifting', 'ESO_DEEP_offset_zapped_spec1D',
-                      '64_271_J024032.03-185139.81_spec1D.fits')
-spec = Table.read(path_2)
-wave_2 = pyasl.vactoair2(spec['wave'])
-flux_2 = spec['flux'] * 1e-3
-model_2 = spec['model'] * 1e-3
-flux_err_2 = spec['error'] * 1e-3
-
-path_3 = os.path.join(os.sep, 'Users', 'lzq', 'Dropbox', 'redshifting', 'ESO_DEEP_offset_zapped_spec1D',
-                      '88_301_J024030.95-185143.68_spec1D.fits')
-spec = Table.read(path_3)
-wave_3 = pyasl.vactoair2(spec['wave'])
-flux_3 = spec['flux'] * 1e-3
-model_3 = spec['model'] * 1e-3
-flux_err_3 = spec['error'] * 1e-3
-
-#
-fig, axarr = plt.subplots(3, 1, figsize=(10, 15), sharex=True, dpi=300)
-plt.subplots_adjust(hspace=0.1)
-axarr[0].plot(wave_1, flux_1, color='k', lw=1)
-axarr[0].plot(wave_1, model_1, color='r', lw=1)
-axarr[0].plot(wave_1, flux_err_1, color='lightgrey')
-axarr[1].plot(wave_2, flux_2, color='k', lw=1)
-axarr[1].plot(wave_2, model_2, color='r', lw=1)
-axarr[1].plot(wave_2, flux_err_2, color='lightgrey')
-axarr[2].plot(wave_3, flux_3, color='k', lw=1)
-axarr[2].plot(wave_3, model_3, color='r', lw=1)
-axarr[2].plot(wave_3, flux_err_3, color='lightgrey')
-axarr[0].set_title('G1', x=0.1, y=0.85, size=20)
-axarr[1].set_title('G64', x=0.1, y=0.85, size=20)
-axarr[2].set_title('G88', x=0.1, y=0.85, size=20)
-axarr[0].minorticks_on()
-axarr[1].minorticks_on()
-axarr[2].minorticks_on()
-axarr[2].set_xlabel(r'$\mathrm{Observed \; Wavelength \; [\AA]}$', size=20)
-axarr[0].set_ylabel(r'${f}_{\lambda}$', size=20)
-axarr[1].set_ylabel(r'${f}_{\lambda}$', size=20)
-axarr[2].set_ylabel(r'${f}_{\lambda}$', size=20)
-axarr[0].tick_params(axis='both', which='major', direction='in', bottom='on', left='on', right='on', labelsize=20, size=5)
-axarr[0].tick_params(axis='both', which='minor', direction='in', bottom='on', left='on', right='on', size=3)
-axarr[1].tick_params(axis='both', which='major', direction='in', bottom='on', left='on', right='on', labelsize=20, size=5)
-axarr[1].tick_params(axis='both', which='minor', direction='in', bottom='on', left='on', right='on', size=3)
-axarr[2].tick_params(axis='both', which='major', direction='in', bottom='on', left='on', right='on', labelsize=20, size=5)
-axarr[2].tick_params(axis='both', which='minor', direction='in', bottom='on', left='on', right='on', size=3)
-fig.savefig('/Users/lzq/Dropbox/Data/CGM_plots/QSP_lunch_talk.png', bbox_inches='tight')
-
 #
 path_hb = os.path.join(os.sep, 'Users', 'lzq', 'Dropbox', 'Data', 'CGM', 'HE0238-1904_drc_offset.fits')
 data_hb = fits.getdata(path_hb, 1, ignore_missing_end=True)
+path_OII_SB = os.path.join(os.sep, 'Users', 'lzq', 'Dropbox', 'Data', 'CGM', 'image_OII_line_SB_offset_revised.fits')
 
 fig = plt.figure(figsize=(8, 8), dpi=300)
 gc = aplpy.FITSFigure(path_hb, figure=fig, north=True)
 gc.set_xaxis_coord_type('scalar')
 gc.set_yaxis_coord_type('scalar')
+gc.show_contour(path_OII_SB, levels=[0.75, 1.5, 3], colors='red')
 gc.recenter(40.1359, -18.8643, width=0.02, height=0.02) # 0.02 / 0.01
 gc.set_system_latex(True)
 gc.show_colorscale(cmap=newcmp)
@@ -104,4 +51,4 @@ gc.show_arrows(xw, yw, -0.00005 * yw, 0, color='k')
 gc.show_arrows(xw, yw, 0, -0.00005 * yw, color='k')
 gc.add_label(0.971, 0.87, r'N', size=15, relative=True)
 gc.add_label(0.912, 0.805, r'E', size=15, relative=True)
-fig.savefig('/Users/lzq/Dropbox/Data/CGM_plots/HST_lunch_talk.png', bbox_inches='tight')
+fig.savefig('/Users/lzq/Dropbox/Data/CGM_plots/Field_Image.png', bbox_inches='tight')
