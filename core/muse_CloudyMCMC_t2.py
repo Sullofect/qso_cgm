f9e79c2b554bc262adc1344944dc0cd9ef72b703
Will Liu <49797040+Sullofect@users.noreply.github.com>
2022-10-06 00:47:10 -0400

    -update

--- core/muse_CloudyMCMC_t2.py
+++ core/muse_CloudyMCMC_t2.py
@@ -24,13 +24,6 @@ path_fit_info_sr = os.path.join(os.sep, 'Users', 'lzq', 'Dropbox', 'Data', 'CGM'
 data_fit_info_sr = fits.getdata(path_fit_info_sr, ignore_missing_end=True)
 data_fit_info_sr = data_fit_info_sr[0]
 
-# t = Table(flux_info, names=('flux_NeV3346', 'flux_NeIII3869', 'flux_HeI3889', 'flux_H8', f'lux_NeIII3968',
-#                             'flux_Heps', 'flux_Hdel', 'flux_Hgam', 'flux_OIII4364', 'flux_HeII4687',
-#                             'flux_OII', 'r_OII', 'flux_Hbeta', 'flux_OIII5008', 'dflux_NeV3346',
-#                             'dflux_NeIII3869', 'dflux_HeI3889', 'dflux_H8', 'dflux_NeIII3968',
-#                             'dflux_Heps', 'dflux_Hdel', 'dflux_Hgam', 'dflux_OIII4364',
-#                             'dflux_HeII4687', 'dflux_OII', 'dr_OII', 'dflux_Hbeta', 'dflux_OIII5008'))
-
 flux_Hbeta, dflux_Hbeta = data_fit_info_sr['flux_Hbeta'], data_fit_info_sr['dflux_Hbeta']
 
 flux_OII, dflux_OII = data_fit_info_sr['flux_OII'] / flux_Hbeta, \
@@ -82,6 +75,7 @@ Hden = np.arange(-2, 2.6, 0.1)  # log
 print(len(Hden))
 metal = np.array([-1.5, -1.4, -1.3, -1.2, -1.1, -1., -0.9, -0.8, -0.7, -0.6, -0.5, -0.4, -0.3,
               -0.2, -0.1, 0., 0.1, 0.2, 0.3, 0.4, 0.5])  # log
+alpha = np.array([-1.8, -1.75, -1.7, -1.65, -1.6, -1.55, -1.5, -1.45, -1.4, -1.35, -1.3, -1.25, -1.2])
 print(len(metal))
 
 # Load lineratio
@@ -96,48 +90,76 @@ def load_cloudy(filename=None, path=None):
     return np.log10(data)
 
 
-def format_cloudy(filename=None, path=None):
-    for i in range(len(filename)):
-        filename_i = 'alpha_1.4_' + str(filename[i])
+def format_cloudy_t2(filename=None, path=None):
+    print(np.shape(filename[0]))
+    print(np.shape(filename[1]))
+    for i in range(len(filename[0])):
+        metal_i = filename[0][i]
+        for j in range(len(filename[1])):
+            alpha_j = filename[1][j]
+            filename_i = 'alpha_' + str(alpha_j) + '_' + str(metal_i)
+            if j == 0:
+                output_j = load_cloudy(filename_i, path=path)
+                ind_j = np.array([[0]])
+            else:
+                ind_jj = np.array([[j]])
+                c_i = load_cloudy(filename_i, path=path)
+                print(np.shape(c_i))
+                print(alpha_j, metal_i)
+                output_j = np.dstack((output_j, c_i))
+                ind_j = np.dstack((ind_j, ind_jj))
+
         if i == 0:
-            output = load_cloudy(filename_i, path=path)
+            ind = ind_j[:, :, :, np.newaxis]
+            output = output_j[:, :, :, np.newaxis]
+            print(np.shape(output))
         else:
-            c_i = load_cloudy(filename_i, path=path)
-            output = np.dstack((output, c_i))
+            print(np.shape(output_j))
+            output = np.concatenate((output, output_j[:, :, :, np.newaxis]), axis=3)
+            ind =  np.concatenate((ind, ind_j[:, :, :, np.newaxis]), axis=3)
+            print(np.shape(output))
     return output
 
-output = format_cloudy(filename=metal, path='/Users/lzq/Dropbox/Data/CGM/cloudy/trial1/')
+output = format_cloudy_t2(filename=[metal, alpha], path='/Users/lzq/Dropbox/Data/CGM/cloudy/trial2/')
 
-f_NeV3346 = interpolate.RegularGridInterpolator((Hden, metal), output[0, :, :], bounds_error=False, fill_value=None)
-f_OII = interpolate.RegularGridInterpolator((Hden, metal), output[1, :, :], bounds_error=False, fill_value=None)
-f_NeIII3869 = interpolate.RegularGridInterpolator((Hden, metal), output[2, :, :], bounds_error=False, fill_value=None)
-f_Hdel = interpolate.RegularGridInterpolator((Hden, metal), output[3, :, :], bounds_error=False, fill_value=None)
-f_Hgam = interpolate.RegularGridInterpolator((Hden, metal), output[4, :, :], bounds_error=False, fill_value=None)
-f_OIII4364 = interpolate.RegularGridInterpolator((Hden, metal), output[5, :, :], bounds_error=False, fill_value=None)
-f_HeII4687 = interpolate.RegularGridInterpolator((Hden, metal), output[6, :, :], bounds_error=False, fill_value=None)
-f_OIII5008 = interpolate.RegularGridInterpolator((Hden, metal), output[7, :, :], bounds_error=False, fill_value=None)
+f_NeV3346 = interpolate.RegularGridInterpolator((Hden, alpha, metal), output[0, :, :, :],
+                                                bounds_error=False, fill_value=None)
+f_OII = interpolate.RegularGridInterpolator((Hden, alpha, metal), output[1, :, :, :],
+                                            bounds_error=False, fill_value=None)
+f_NeIII3869 = interpolate.RegularGridInterpolator((Hden, alpha, metal), output[2, :, :, :],
+                                                  bounds_error=False, fill_value=None)
+f_Hdel = interpolate.RegularGridInterpolator((Hden, alpha, metal), output[3, :, :, :],
+                                             bounds_error=False, fill_value=None)
+f_Hgam = interpolate.RegularGridInterpolator((Hden, alpha, metal), output[4, :, :, :],
+                                             bounds_error=False, fill_value=None)
+f_OIII4364 = interpolate.RegularGridInterpolator((Hden, alpha, metal), output[5, :, :, :],
+                                                 bounds_error=False, fill_value=None)
+f_HeII4687 = interpolate.RegularGridInterpolator((Hden, alpha, metal), output[6, :, :, :],
+                                                 bounds_error=False, fill_value=None)
+f_OIII5008 = interpolate.RegularGridInterpolator((Hden, alpha, metal), output[7, :, :, :],
+                                                 bounds_error=False, fill_value=None)
 
 # Define the log likelihood function and run MCMC
 def log_prob(x):
-    logden, logz = x[0], x[1]
-    # if logden < 1:
+    logden, alpha, logz = x[0], x[1], x[2]
+    # if alpha > -1.2:
     #     return -np.inf
     # else:
-    return - 0.5 * (((f_NeV3346((logden, logz)) - logflux_NeV3346) / dlogflux_NeV3346) ** 2
-                    + ((f_OII((logden, logz)) - logflux_OII) / dlogflux_OII) ** 2
-                    + ((f_NeIII3869((logden, logz)) - logflux_NeIII3869) / dlogflux_NeIII3869) ** 2
-                    + ((f_Hdel((logden, logz)) - logflux_Hdel) / dlogflux_Hdel) ** 2
-                    + ((f_Hgam((logden, logz)) - logflux_Hgam) / dlogflux_Hgam) ** 2
-                    + ((f_OIII4364((logden, logz)) - logflux_OIII4364) / dlogflux_OIII4364) ** 2
-                    + ((f_HeII4687((logden, logz)) - logflux_HeII4687) / dlogflux_HeII4687) ** 2
-                    + ((f_OIII5008((logden, logz)) - logflux_OIII5008) / dlogflux_OIII5008) ** 2)
+    return - 0.5 * (((f_NeV3346((logden, alpha, logz)) - logflux_NeV3346) / dlogflux_NeV3346) ** 2
+                    + ((f_OII((logden, alpha, logz)) - logflux_OII) / dlogflux_OII) ** 2
+                    + ((f_NeIII3869((logden, alpha, logz)) - logflux_NeIII3869) / dlogflux_NeIII3869) ** 2
+                    + ((f_Hdel((logden, alpha, logz)) - logflux_Hdel) / dlogflux_Hdel) ** 2
+                    + ((f_Hgam((logden, alpha, logz)) - logflux_Hgam) / dlogflux_Hgam) ** 2
+                    + ((f_OIII4364((logden, alpha, logz)) - logflux_OIII4364) / dlogflux_OIII4364) ** 2
+                    + ((f_HeII4687((logden, alpha, logz)) - logflux_HeII4687) / dlogflux_HeII4687) ** 2
+                    + ((f_OIII5008((logden, alpha, logz)) - logflux_OIII5008) / dlogflux_OIII5008) ** 2)
 
 
-ndim, nwalkers = 2, 40
-p0 = np.array([1.1, -0.3]) + 0.1 * np.random.randn(nwalkers, ndim)
+ndim, nwalkers = 3, 40
+p0 = np.array([1.1, -1.4, -0.3]) + 0.1 * np.random.randn(nwalkers, ndim)
 sampler = emcee.EnsembleSampler(nwalkers, ndim, log_prob)
-state = sampler.run_mcmc(p0, 10000)
-samples = sampler.get_chain(flat=True, discard=1000)
+state = sampler.run_mcmc(p0, 1000)
+samples = sampler.get_chain(flat=True, discard=100)
 
 # chain_emcee = sampler.get_chain()
 # f, ax = plt.subplots(1, 2, figsize=(15, 7))
@@ -151,7 +173,8 @@ samples = sampler.get_chain(flat=True, discard=1000)
 
 #
 # ax = plt.figure(figsize=(10, 10), dpi=300)
-figure = corner.corner(samples, labels=[r"$\mathrm{log_{10}(n)}$", r"$\mathrm{log_{10}(Z/Z_{\odot})}$"],
+figure = corner.corner(samples, labels=[r"$\mathrm{log_{10}(n)}$", r"$\mathrm{\alpha}$",
+                                        r"$\mathrm{log_{10}(Z/Z_{\odot})}$"],
                        quantiles=[0.16, 0.5, 0.84], show_titles=True, color='k', title_kwargs={"fontsize": 13},
                        smooth=1., smooth1d=1., bins=25)
 
@@ -161,40 +184,40 @@ for i, ax in enumerate(figure.get_axes()):
     ax.tick_params(axis='both', direction='in', top='on', bottom='on')
 figure.savefig('/Users/lzq/Dropbox/Data/CGM_plots/Cloudy_test_mcmc_all.pdf', bbox_inches='tight')
 
-
-NeV3346_NeIII3869_error = np.sqrt(dlogflux_NeV3346 ** 2 + dlogflux_NeIII3869 ** 2)
-OIII5008_OII_error = np.sqrt(dlogflux_OIII5008 ** 2 + dlogflux_OII ** 2)
-fig, ax = plt.subplots(3, 1, figsize=(5, 12), dpi=300, sharex=True)
-for z_i in np.array([-1.5, -1., -0.5, 0, 0.3, 0.4, 0.5]):
-    i = np.where(metal == z_i)
-    i = i[0][0]
-    ax[0].plot(Hden, output[0, :, i] - output[2, :, i], label="Z = " + str(z_i))
-    ax[0].plot(Hden, f_NeV3346((Hden, z_i)) - f_NeIII3869((Hden, z_i)), '--k', lw=0.2)
-    ax[1].plot(Hden, output[7, :, i] - output[1, :, i], label="Z = " + str(z_i))
-    ax[1].plot(Hden, f_OIII5008((Hden, z_i)) - f_OII((Hden, z_i)), '--k', lw=0.2)
-    ax[2].plot(Hden, output[6, :, i], label="Z = " + str(z_i))
-    ax[2].plot(Hden, f_HeII4687((Hden, z_i)), '--k', lw=0.2)
-ax[0].axhline(logflux_NeV3346 - logflux_NeIII3869, xmin=-2, xmax=2.6, ls='--', color='r')
-ax[1].axhline(logflux_OIII5008 - logflux_OII, xmin=-2, xmax=2.6, ls='--', color='r')
-ax[2].axhline(logflux_HeII4687 - logflux_Hbeta, xmin=-2, xmax=2.6, ls='--', color='r')
-ax[0].minorticks_on()
-ax[1].minorticks_on()
-ax[2].minorticks_on()
-ax[0].set_xlim(-2.5, 3)
-# ax[1].set_ylim(-50, 50)
-ax[2].set_xlabel(r'$\mathrm{log(n)}$', size=20)
-ax[0].set_ylabel(r'$\mathrm{log(\frac{[Ne \, V]}{[Ne \, III])}}$', size=20)
-ax[1].set_ylabel(r'$\mathrm{log(\frac{[O \, III]}{[O \, II])}}$', size=20)
-ax[2].set_ylabel(r'$\mathrm{log(\frac{[He \, II]}{H\beta)}}$', size=20)
-ax[0].tick_params(axis='both', which='major', direction='in', bottom='on', left='on', right='on', labelsize=20, size=5)
-ax[0].tick_params(axis='both', which='minor', direction='in', bottom='on', left='on', right='on', size=3)
-ax[1].tick_params(axis='both', which='major', direction='in', bottom='on', left='on', right='on', labelsize=20, size=5)
-ax[1].tick_params(axis='both', which='minor', direction='in', bottom='on', left='on', right='on', size=3)
-ax[2].tick_params(axis='both', which='major', direction='in', bottom='on', left='on', right='on', labelsize=20, size=5)
-ax[2].tick_params(axis='both', which='minor', direction='in', bottom='on', left='on', right='on', size=3)
-ax[0].legend()
-fig.tight_layout()
-fig.savefig('/Users/lzq/Dropbox/Data/CGM_plots/Cloudy_check_MCMC.png', bbox_inches='tight')
+#
+# NeV3346_NeIII3869_error = np.sqrt(dlogflux_NeV3346 ** 2 + dlogflux_NeIII3869 ** 2)
+# OIII5008_OII_error = np.sqrt(dlogflux_OIII5008 ** 2 + dlogflux_OII ** 2)
+# fig, ax = plt.subplots(3, 1, figsize=(5, 12), dpi=300, sharex=True)
+# for z_i in np.array([-1.5, -1., -0.5, 0, 0.3, 0.4, 0.5]):
+#     i = np.where(metal == z_i)
+#     i = i[0][0]
+#     ax[0].plot(Hden, output[0, :, i] - output[2, :, i], label="Z = " + str(z_i))
+#     ax[0].plot(Hden, f_NeV3346((Hden, z_i)) - f_NeIII3869((Hden, z_i)), '--k', lw=0.2)
+#     ax[1].plot(Hden, output[7, :, i] - output[1, :, i], label="Z = " + str(z_i))
+#     ax[1].plot(Hden, f_OIII5008((Hden, z_i)) - f_OII((Hden, z_i)), '--k', lw=0.2)
+#     ax[2].plot(Hden, output[6, :, i], label="Z = " + str(z_i))
+#     ax[2].plot(Hden, f_HeII4687((Hden, z_i)), '--k', lw=0.2)
+# ax[0].axhline(logflux_NeV3346 - logflux_NeIII3869, xmin=-2, xmax=2.6, ls='--', color='r')
+# ax[1].axhline(logflux_OIII5008 - logflux_OII, xmin=-2, xmax=2.6, ls='--', color='r')
+# ax[2].axhline(logflux_HeII4687 - logflux_Hbeta, xmin=-2, xmax=2.6, ls='--', color='r')
+# ax[0].minorticks_on()
+# ax[1].minorticks_on()
+# ax[2].minorticks_on()
+# ax[0].set_xlim(-2.5, 3)
+# # ax[1].set_ylim(-50, 50)
+# ax[2].set_xlabel(r'$\mathrm{log(n)}$', size=20)
+# ax[0].set_ylabel(r'$\mathrm{log(\frac{[Ne \, V]}{[Ne \, III])}}$', size=20)
+# ax[1].set_ylabel(r'$\mathrm{log(\frac{[O \, III]}{[O \, II])}}$', size=20)
+# ax[2].set_ylabel(r'$\mathrm{log(\frac{[He \, II]}{H\beta)}}$', size=20)
+# ax[0].tick_params(axis='both', which='major', direction='in', bottom='on', left='on', right='on', labelsize=20, size=5)
+# ax[0].tick_params(axis='both', which='minor', direction='in', bottom='on', left='on', right='on', size=3)
+# ax[1].tick_params(axis='both', which='major', direction='in', bottom='on', left='on', right='on', labelsize=20, size=5)
+# ax[1].tick_params(axis='both', which='minor', direction='in', bottom='on', left='on', right='on', size=3)
+# ax[2].tick_params(axis='both', which='major', direction='in', bottom='on', left='on', right='on', labelsize=20, size=5)
+# ax[2].tick_params(axis='both', which='minor', direction='in', bottom='on', left='on', right='on', size=3)
+# ax[0].legend()
+# fig.tight_layout()
+# fig.savefig('/Users/lzq/Dropbox/Data/CGM_plots/Cloudy_check_MCMC.png', bbox_inches='tight')
 
 
 # Check
@@ -213,7 +236,7 @@ data_yerr = [np.sqrt(dlogflux_OII ** 2 + dlogflux_Hbeta ** 2),
              np.sqrt(dlogflux_HeII4687 ** 2 + dlogflux_Hbeta ** 2),
              np.sqrt(dlogflux_NeV3346 ** 2 + dlogflux_Hbeta ** 2)]
 ax.errorbar(data_x, data_y,  data_yerr, fmt='.k', capsize=2, elinewidth=1, mfc='red', ms=10)
-best_fit = (1.49, -0.19)
+best_fit = (1.49, -0.79, -0.19)
 bestfit_y = [f_OII(best_fit), f_OIII4364(best_fit), f_OIII5008(best_fit), f_NeIII3869(best_fit), f_HeII4687(best_fit),
              f_NeV3346(best_fit)]
 ax.plot(data_x, bestfit_y, '-k', alpha=1)
@@ -221,7 +244,7 @@ ax.plot(data_x, bestfit_y, '-k', alpha=1)
 inds = np.random.randint(len(samples), size=50)
 for i in inds:
     sample = samples[i]
-    model = (sample[0], sample[1])
+    model = (sample[0], sample[1], sample[2])
     model_y = [f_OII(model), f_OIII4364(model), f_OIII5008(model), f_NeIII3869(model), f_HeII4687(model),
                f_NeV3346(model)]
     ax.plot(data_x, model_y, '-C1', alpha=0.1)
