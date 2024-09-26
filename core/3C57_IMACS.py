ae2ac9d4f38dc8273ff67338c066da9bbe8655fe
Will Liu <49797040+Sullofect@users.noreply.github.com>
2024-09-26 19:18:09 -0400

    -update

--- core/3C57_IMACS.py
+++ core/3C57_IMACS.py
@@ -20,12 +20,28 @@ from photutils.background import Background2D, MedianBackground
 from photutils.segmentation import detect_sources, SourceCatalog, deblend_sources
 from astropy.convolution import Kernel, convolve, Gaussian2DKernel
 
+# 3C57
+cubename = '3C57'
+path_qso = '../../MUSEQuBES+CUBS/gal_info/quasars.dat'
+data_qso = ascii.read(path_qso, format='fixed_width')
+data_qso = data_qso[data_qso['name'] == cubename]
+ra_qso, dec_qso, z_qso = data_qso['ra_GAIA'][0], data_qso['dec_GAIA'][0], data_qso['redshift'][0]
+
 # IMACS object files
 obj_3C57i1 = fits.open('../../MUSEQuBES+CUBS/IMACS+LDSS3/3C57/3C57i1_objects.fits')
 obj_3C57i2 = fits.open('../../MUSEQuBES+CUBS/IMACS+LDSS3/3C57/3C57i2_objects.fits')
 obj_3C57i3 = fits.open('../../MUSEQuBES+CUBS/IMACS+LDSS3/3C57/3C57i3_objects.fits')
 dat_3C57 = ascii.read('../../MaskDesign/3C57_mask/3C57_@_ac.dat')
 obj_3C57_LDSS = fits.open('../../MUSEQuBES+CUBS/IMACS+LDSS3/3C57/3C57_DES_redshifts.fits')
+
+# LDSS3
+ra_LDSS, dec_LDSS, z_LDSS = obj_3C57_LDSS[1].data['RA'], obj_3C57_LDSS[1].data['DEC'], obj_3C57_LDSS[1].data['REDSHIFT']
+v_LDSS = 3e5 * (z_LDSS - 0.6718) / (1 + 0.6718)
+mask = (v_LDSS < 2000) * (v_LDSS > -2000)
+ra_group, dec_group, z_group = ra_LDSS[mask], dec_LDSS[mask], z_LDSS[mask]
+coord_group = SkyCoord(ra_group * u.deg, dec_group * u.deg, frame='icrs')
+sep_group = coord_group.separation(SkyCoord(ra_qso * u.deg, dec_qso * u.deg, frame='icrs'))
+print(z_group, sep_group.arcsec)
 
 # IMACS
 z_i1, z_i2, z_i3 = obj_3C57i1[1].data['redshift'], obj_3C57i2[1].data['redshift'], obj_3C57i3[1].data['redshift']
@@ -34,13 +50,6 @@ z_total = np.hstack((z_i1, z_i2, z_i3))
 id_total = np.hstack((id_z1, id_z2, id_z3))
 v_total = 3e5 * (z_total - 0.6718) / (1 + 0.6718)
 
-# LDSS3
-ra_LDSS, dec_LDSS, z_LDSS = obj_3C57_LDSS[1].data['RA'], obj_3C57_LDSS[1].data['DEC'], obj_3C57_LDSS[1].data['REDSHIFT']
-v_LDSS = 3e5 * (z_LDSS - 0.6718) / (1 + 0.6718)
-mask = (v_LDSS < 2000) * (v_LDSS > -2000)
-ra_group, dec_group, z_group = ra_LDSS[mask], dec_LDSS[mask], z_LDSS[mask]
-
-
 #
 id, ra, dec = dat_3C57['col1'], np.asarray(dat_3C57['col2']), np.asarray(dat_3C57['col3'])
 mask = (v_total < 2000) * (v_total > -2000)
@@ -48,9 +57,11 @@ id_mask = id_total[mask]
 id_mask = np.array(['@' + i for i in id_mask])
 overlap = np.in1d(id, id_mask)
 ra_overlap, dec_overlap = ra[overlap], dec[overlap]
+z_overlap = z_total[mask]
 
 coord = SkyCoord(ra_overlap, dec_overlap, unit=(u.hour, u.deg), frame='icrs')
-print(coord.ra.deg, coord.dec.deg)
+sep = coord.separation(SkyCoord(ra_qso * u.deg, dec_qso * u.deg, frame='icrs'))
+print(z_overlap, sep.arcsec)
 #
 # plt.figure()
 # plt.hist(z_total[mask])
@@ -64,10 +75,10 @@ t['z'] = z_total[mask]
 t['v'] = v_total[mask]
 t.write(filename, format='fits', overwrite=True)
 
-cubename = '3C57'
 path_gal = '../../MUSEQuBES+CUBS/gal_info/{}_gal_info_gaia.fits'.format(cubename)
 data_gal = fits.open(path_gal)[1].data
 ra_gal, dec_gal, v_gal = data_gal['ra'], data_gal['dec'], data_gal['v']
+raise ValueError('Stop here')
 
 # Check SDSS image
 fig = plt.figure(figsize=(8, 8), dpi=300)
