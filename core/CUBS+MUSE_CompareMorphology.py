3ebb63dfef644cdf574e788b2a863c1b6d82eb5b
Will Liu <49797040+Sullofect@users.noreply.github.com>
2025-05-15 21:28:51 -0400

    -update

--- core/CUBS+MUSE_CompareMorphology.py
+++ core/CUBS+MUSE_CompareMorphology.py
@@ -38,20 +38,23 @@ rc('ytick.major', size=8)
 
 # Save the asymmetry values
 path_OII_asymmetry = '../../MUSEQuBES+CUBS/asymmetry/CUBS+MUSE_OII_asymmetry.txt'
-t = Table.read(path_OII_asymmetry, format='ascii.fixed_width')
+t_OII = Table.read(path_OII_asymmetry, format='ascii.fixed_width')
+
+path_OIII_asymmetry = '../../MUSEQuBES+CUBS/asymmetry/CUBS+MUSE_OIII_asymmetry.txt'
+t_OIII = Table.read(path_OIII_asymmetry, format='ascii.fixed_width')
 
 path_21cm_asymmetry = '../../MUSEQuBES+CUBS/asymmetry/CUBS+MUSE_21cm_asymmetry.txt'
 t_21cm = Table.read(path_21cm_asymmetry, format='ascii.fixed_width')
 
 bins = np.linspace(0, 2, 11)
 plt.figure(figsize=(5, 5), dpi=300)
-plt.hist(t['A_shape_ZQL'], bins=bins, color='blue', alpha=0.5, label='OII asymmetry')
-plt.hist(t_21cm['A_shape_ZQL'], bins=bins, color='red', alpha=0.5, label='21cm asymmetry', histtype='step')
+plt.hist(t_OII['A_shape_ZQL'], bins=bins, color='brown', alpha=0.5, label=r'$\rm [O\,II]$')
+plt.hist(t_OIII['A_shape_ZQL'], bins=bins, color='blue', alpha=0.5, label=r'$\rm [O\,III]$')
+plt.hist(t_21cm['A_shape_ZQL'], bins=bins, color='red', alpha=1.0, label=r'$\rm HI \, 21\,cm$', histtype='step', lw=2)
 plt.xlim(0, 2)
 plt.xlabel('Asymmetry', size=20)
 plt.ylabel('N', size=20)
 plt.legend(loc='upper right', fontsize=15)
-# plt.title('Asymmetry distribution')
 plt.savefig('../../MUSEQuBES+CUBS/plots/CUBS+MUSE_asymmetry_distribution.png', bbox_inches='tight')
 
 
