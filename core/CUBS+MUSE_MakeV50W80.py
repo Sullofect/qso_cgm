5339327e822aef78d94cf8995ba9eeac0a08aa25
Will Liu <49797040+Sullofect@users.noreply.github.com>
2025-06-03 00:15:05 -0400

    -update

--- core/CUBS+MUSE_MakeV50W80.py
+++ core/CUBS+MUSE_MakeV50W80.py
@@ -447,8 +447,8 @@ def MakeV50W80(cubename=None, v_max=300, sigma_max=300, contour_level_OII=0.2, c
 # MakeV50W80(cubename='PKS0552-640', v_max=300, sigma_max=300, contour_level_OII=0.2, contour_level_OIII=0.2,
 #            nums_seg_OII=[2, 6, 7, 9, 10, 14, 18], nums_seg_OIII=[7, 9, 12, 19, 20], rmbkgResidue=True)
 # MakeV50W80(cubename='J0110-1648', v_max=300, sigma_max=300, rmbkgResidue=True)
-# MakeV50W80(cubename='J0454-6116', v_max=500, sigma_max=400, nums_seg_OII=[2, 6, 8, 13, 17, 18],
-#            nums_seg_OIII=[2, 9, 10, 18])
+MakeV50W80(cubename='J0454-6116', v_max=500, sigma_max=400, nums_seg_OII=[2, 6, 8, 13, 17, 18],
+           nums_seg_OIII=[2, 9, 10, 18])
 # MakeV50W80(cubename='J2135-5316', v_max=300, sigma_max=300, contour_level_OII=0.3,
 #            nums_seg_OII=[3, 4, 10, 12, 13, 14, 16, 17, 18, 19],
 #            nums_seg_OIII=[4, 12, 13, 14, 15, 17, 19, 20], rmbkgResidue=True) # Double component # SNR=5
@@ -457,7 +457,7 @@ def MakeV50W80(cubename=None, v_max=300, sigma_max=300, contour_level_OII=0.2, c
 #            rmbkgResidue=True)  # Double component
 # MakeV50W80(cubename='HE0246-4101', v_max=300, sigma_max=300)
 # MakeV50W80(cubename='J0028-3305', v_max=300, sigma_max=300, contour_level_OII=0.3)
-MakeV50W80(cubename='HE0419-5657', v_max=400, sigma_max=300, nums_seg_OII=[1], rmbkgResidue=True)
+# MakeV50W80(cubename='HE0419-5657', v_max=400, sigma_max=300, nums_seg_OII=[1], rmbkgResidue=True)
 # MakeV50W80(cubename='PB6291', v_max=400, sigma_max=300, nums_seg_OII=[3, 5], HSTcentroid=True)
 # MakeV50W80(cubename='Q0107-0235', v_max=400, sigma_max=300, nums_seg_OII=[7], rmbkgResidue=True, HSTcentroid=True)
 # MakeV50W80(cubename='PKS2242-498', v_max=400, sigma_max=300, contour_level_OII=0.3)
